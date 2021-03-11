38a53213b05d8bf863b850608d5ff33c1df2c21b
vicente <vicentelcandia@gmail.com>
2021-03-10 21:40:33 -0300

    adding stuff

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ app = Flask(__name__);
 def index():
     return render_template('index.html')
 
+@app.route("/info")
+
+def info():
+    return render_template('info.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
